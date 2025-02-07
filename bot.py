3b18310e5762845f167cdf2a717713d1d0af3821
Serhii Kroshka <pm012@ukr.net>
2025-02-07 21:22:46 +0200

    Finished working on telegram bot

--- bot.py
+++ bot.py
@@ -15,9 +15,6 @@ if not TOKEN:
 if not OPEN_AI_TOKEN:
     raise ValueError("Open AI token is missing. Check your .env file.")
 
-
-# тут будемо писати наш код :)
-
 async def start(update, context):    
     msg = load_message("main")
     await send_photo(update, context, "main")
@@ -56,7 +53,6 @@ async def date(update, context):
         "date_hardy": "Tom Hardy",
     })
     
-
 async def date_button(update, context):
     query = update.callback_query.data    
     await update.callback_query.answer()
@@ -70,7 +66,6 @@ async def date_dialog(update, context):
     my_message = await send_text(update, context, "Набирає повідомлення...")
     answer = await chatgpt.add_message(text)
     await my_message.edit_text(answer)
-    #await send_text(update, context, answer)
     
 async def message(update, context):
     dialog.mode = "message"
@@ -99,22 +94,98 @@ async def message_button(update, context):
     my_message  = await send_text(update, context, "Думаю над варіантами...")
     answer = await chatgpt.send_question(prompt, user_chat_history)
     await my_message.edit_text(answer)
+ 
+async def profile(update, context):
+    dialog.mode = "profile"
+    msg = load_message("profile")
+    await send_photo(update, context, "profile")
+    await send_text(update, context, msg)
     
+    dialog.user.clear()
+    dialog.counter = 0
     
-async def hello(update, context):
-    #await send_text(update, context, "Hi, user! " + update.message.text)
+    await send_text(update, context, "Скільки вам років?")
+ 
+async def profile_dialog(update, context):
+    text = update.message.text
+    dialog.counter += 1
+    
+    if dialog.counter == 1:
+        dialog.user["age"] = text
+        await send_text(update, context, "Ким ви працюєте?")
+    if dialog.counter == 2:
+        dialog.user["occupation"] = text
+        await send_text(update, context, "У вас є хобі?")
+    if dialog.counter == 3:
+        dialog.user["hobby"] = text
+        await send_text(update, context, "Що вам не подобається в людях?")
+    if dialog.counter == 4:
+        dialog.user["annoys"] = text
+        await send_text(update, context, "Мета знайомства?")
+    if dialog.counter == 5:
+        dialog.user["goals"] = text
+        prompt = load_prompt("profile")
+        user_info = dialog_user_info_to_str(dialog.user)
+        
+        my_message = await send_text(update, context, "ChatGPT \uD83E\uDDE0 генерує ваш профіль. Зачекайте кілька секунд.")
+        answer = await chatgpt.send_question(prompt, user_info)
+        await my_message.edit_text(answer)
+        
+
+async def opener(update, context):
+    dialog.mode = "opener"
+    msg = load_message("opener")
+    await send_photo(update, context, "opener")
+    await send_text(update, context, msg)
+    
+    dialog.user.clear()
+    dialog.counter = 0
+    
+    await send_text(update, context, "Ім'я партнера?")
+    
+async def opener_dialog(update, context):
+    text = update.message.text
+    dialog.counter += 1
+    
+    if dialog.counter == 1:
+        dialog.user["name"] = text
+        await send_text(update, context, "Скільки років партнеру?")
+    if dialog.counter == 2:
+        dialog.user["age"] = text
+        await send_text(update, context, "Оцініть зовнішність: 1-10 балів?")
+    if dialog.counter == 3:
+        dialog.user["handsome"] = text
+        await send_text(update, context, "Ким вона працює?")
+    if dialog.counter == 4:
+        dialog.user["occupation"] = text
+        await send_text(update, context, "Мета знайомства?")
+    if dialog.counter == 5:
+        dialog.user["goals"] = text
+        prompt = load_prompt("opener")
+        user_info = dialog_user_info_to_str(dialog.user)
+        
+        my_message = await send_text(update, context, "ChatGPT \uD83E\uDDE0 генерує вашe повідомлення...")
+        answer = await chatgpt.send_question(prompt, user_info)
+        await my_message.edit_text(answer)
+   
+async def hello(update, context):    
     if dialog.mode == "gpt":
         await gpt_dialog(update, context)
     elif dialog.mode == "date":
         await date_dialog(update, context)    
     elif dialog.mode == "message":
         await message_dialog(update, context)
-    
+    elif dialog.mode == "profile":
+        await profile_dialog(update, context)
+    elif dialog.mode == "opener":
+        await opener_dialog(update, context)    
     
     
 dialog = Dialog()
 dialog.mode = None
 dialog.list = []
+dialog.user = {}
+dialog.counter = 0
 
 chatgpt = ChatGptService(token=OPEN_AI_TOKEN)
 
@@ -123,6 +194,8 @@ app.add_handler(CommandHandler("start", start))
 app.add_handler(CommandHandler("gpt", gpt))
 app.add_handler(CommandHandler("date", date))
 app.add_handler(CommandHandler("message", message))
+app.add_handler(CommandHandler("profile", profile))
+app.add_handler(CommandHandler("opener", opener))
 app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, hello))
 app.add_handler(CallbackQueryHandler(date_button, pattern="^date_."))
 app.add_handler(CallbackQueryHandler(message_button, pattern="^message_."))
